7c74dfa673bbb6f94d5d61492a2d36ce162cf950
Stuart Jones <stuart@savvywombat.com.au>
2020-06-14 09:30:03 +0930

    production css is purged and reduced

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
+const cssnano = require('cssnano');
+const purgecss = require('postcss-purgecss');
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+        process.env.NODE_ENV === 'production'
+            ? cssnano({
+                preset: 'default'
+            })
+            : null,
+        purgecss({
+            content: [
+                './src/**/*.html',
+                './src/**/*.vue'
+            ],
+            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
     ]
 };
